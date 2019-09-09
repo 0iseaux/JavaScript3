975b7ff80b5d1da257a9cd432392ffa56b954fab
jenny <jennyshngai@gmail.com>
2019-09-09 13:29:42 +0200

    fixed branch issues

--- homework/index.js
+++ homework/index.js
@@ -1,7 +1,7 @@
 'use strict';
-/* eslint-disable */
-// fetchJSON	Uses XMLHttpRequest to fetch JSON data from an API end point. This function uses an asynchronous callback.
-// It is not likely that you will need to modify fetchJSON().
+// Replace XMLHttpRequest with the fetch API.
+// Refactor all.then() and.catch() methods with async / await and try...catch.
+
 const root = document.getElementById('root');
 const select = document.querySelector('select');
 const selected = document.getElementById('selected');
@@ -18,14 +18,8 @@ function createAndAppend(name, parent, options = {}) {
     Object.keys(options).forEach(key => {
         const value = options[key];
 
-        /* testing lines start 
-                console.log(options) // {text: "AngularJS"}
-                console.log(key) // text
-                console.log(value) // AngularJS
-                testing lines end */
-
         if (key === 'text') {
-            elem.innerHTML = value; //changed 'textContent' to innerHTML
+            elem.innerHTML = value;
         } else {
             elem.setAttribute(key, value);
         }
@@ -33,47 +27,22 @@ function createAndAppend(name, parent, options = {}) {
     return elem;
 }
 
-// main contains the start-up code for the application.
-// Add new functions and modify function main() as you see fit.
-// https://stackoverflow.com/questions/54656223/fetch-function-return-promise-pending
+async function getAndSortAndShowRepoData(url) {
+    try {
+        const bigRepoData = fetch(url);
+        const repoResponse = await bigRepoData;
+        const data = await repoResponse.json();
 
-// https://codereview.stackexchange.com/questions/123577/using-fetch-and-a-new-promise-object-to-get-api-results
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-
-function main(url) {
-    const fetchJSON = async function fetchJSON(url) {
-        const fetchRepoData = await fetch(url);
-        const repoData = await fetchRepoData.json();
-        console.log(repoData);
-        sortRepo(repoData);
-        /*
-        try { // if(response.ok)...
-            const fetchRepoData = await fetch(url);
-            console.log(fetchRepoData);
-            const repoData = await fetchRepoData.json();
-            console.log(repoData);
-            sortRepo(repoData);
-        } catch (e) {
-            createAndAppend('div', root, {
-                text: err.message,
-                class: 'alert-error',
-            });
-        }
-        */
-    }
-
-
-    //SORT repo names in alphabetical order (case-insensitive) & generate obj with REDUCE
-    function sortRepo(repoData) {
-        let sorted = Object.keys(repoData)
+        //SORT repo names in alphabetical order (case-insensitive) & generate obj with REDUCE
+        let sorted = Object.keys(data)
             .sort((a, b) => {
-                a = repoData[a].name.toLowerCase();
-                b = repoData[b].name.toLowerCase();
+                a = data[a].name.toLowerCase();
+                b = data[b].name.toLowerCase();
                 if (a === b) return 0;
                 return a < b ? -1 : 1;
             })
             .reduce((acc, curr, i) => {
-                acc[i] = repoData[curr];
+                acc[i] = data[curr];
                 return acc;
             }, {});
         // console.log(sorted);
@@ -83,88 +52,88 @@ function main(url) {
             repoNamesArr.push(sorted[repo].name);
         }
         // console.log(repoNamesArr);
-        appendRepoData(repoNamesArr)
-    }
 
-    // create list of repo names from Arr to select from
-    function appendRepoData(repoNamesArr) {
+        // create list of repo names from Arr to select from
         repoNamesArr.forEach(repoName => {
             createAndAppend('option', select, {
                 text: repoName,
                 value: repoNamesArr.indexOf(repoName), // where each option element has the array index of the repository as its value
             });
         });
-    }
 
-    // WHEN SELECTING A REPO ...
-    select.addEventListener('change', repo => {
-        // SHOW repo info
-        selected.innerHTML = '';
 
-        /* ref 
-                  console.log(repo);
-                  console.log(repo.target);
-                  console.log(repo.target.value);
-                  console.log(sorted[repo.target.value].url);
-                  */
+        // WHEN SELECTING A REPO ...
+        select.addEventListener('change', repo => {
+            // SHOW repo info
+            selected.innerHTML = '';
 
-        createAndAppend('div', selected, {
-            text: `<b>Name of Repository:</b><br><a href="${
-              sorted[repo.target.value].html_url
-            }" target="_blank">${sorted[repo.target.value].name}</a><br><br>`,
-            class: 'selected',
-        });
-        createAndAppend('div', selected, {
-            text: `<b>Description:</b><br>${sorted[repo.target.value].description}<br><br>`,
-            class: 'selected',
-        });
-        createAndAppend('div', selected, {
-            text: `<b>Fork(s)</b>:<br>${sorted[repo.target.value].forks}<br><br>`,
-            class: 'selected',
-        });
-        createAndAppend('div', selected, {
-            text: `<b>Updated at</b>:<br>${sorted[repo.target.value].updated_at}<br><br>`,
-            class: 'selected',
-        });
+            /* ref 
+                      console.log(repo);
+                      console.log(repo.target);
+                      console.log(repo.target.value);
+                      console.log(sorted[repo.target.value].url);
+                      */
 
-        // FETCH contributors
-        // ref: console.log(sorted[repo.target.value].contributors_url)
-        fetch(sorted[repo.target.value].contributors_url)
-            .then(response => response.json())
-            .catch(err => {
-                createAndAppend('div', root, {
-                    text: err.message,
-                    class: 'alert-error',
-                });
-            })
-
-            .then(contributorsData => {
-                // console.log(contributorsData)
-
-                contributors.innerHTML = '';
-
-                for (const i in contributorsData) {
-                    createAndAppend('div', contributors, {
-                        text: `<img src="${contributorsData[i].avatar_url}">`,
-                        class: 'contributors',
-                    });
-                    createAndAppend('div', contributors, {
-                        text: `<b>Name of Contributor:</b><a href="${
-                    contributorsData[i].html_url
-                  }" target="_blank">${contributorsData[i].login}</a>`,
-                        class: 'contributors',
-                    });
-                    createAndAppend('div', contributors, {
-                        text: `<b>Contributions:</b>${contributorsData[i].contributions}</a>`,
-                        class: 'contributors',
-                    });
-                }
+            createAndAppend('div', selected, {
+                text: `<b>Name of Repository:</b><br><a href="${
+                                sorted[repo.target.value].html_url
+                                }" target="_blank">${sorted[repo.target.value].name}</a><br><br>`,
+                class: 'selected',
             });
-    });
-};
+            createAndAppend('div', selected, {
+                text: `<b>Description:</b><br>${sorted[repo.target.value].description}<br><br>`,
+                class: 'selected',
+            });
+            createAndAppend('div', selected, {
+                text: `<b>Fork(s)</b>:<br>${sorted[repo.target.value].forks}<br><br>`,
+                class: 'selected',
+            });
+            createAndAppend('div', selected, {
+                text: `<b>Updated at</b>:<br>${sorted[repo.target.value].updated_at}<br><br>`,
+                class: 'selected',
+            });
+
+            getAndShowContributorsData(sorted[repo.target.value].contributors_url);
+
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function getAndShowContributorsData(url) {
+    // FETCH contributors
+    // ref: console.log(sorted[repo.target.value].contributors_url)
+    try {
+        const bigContributorsData = fetch(url);
+        const contributorsResponse = await bigContributorsData;
+        const contributorsData = await contributorsResponse.json();
+
+        contributors.innerHTML = '';
+
+        for (const i in contributorsData) {
+            createAndAppend('div', contributors, {
+                text: `<img src="${contributorsData[i].avatar_url}">`,
+                class: 'contributors',
+            });
+            createAndAppend('div', contributors, {
+                text: `<b>Name of Contributor:</b><a href="${
+                    contributorsData[i].html_url
+                    }" target="_blank">${contributorsData[i].login}</a>`,
+                class: 'contributors',
+            });
+            createAndAppend('div', contributors, {
+                text: `<b>Contributions:</b>${contributorsData[i].contributions}</a>`,
+                class: 'contributors',
+            });
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
 
 
 const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 window.onload = () => {
-    main(HYF_REPOS_URL);
+    getAndSortAndShowRepoData(HYF_REPOS_URL);
 };
