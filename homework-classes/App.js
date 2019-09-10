8249af1b61f804d6e48f9fb83027fecbf492e1d5
jenny <jennyshngai@gmail.com>
2019-09-11 01:28:03 +0200

    sorted & rendered reponames on <select>

--- homework-classes/App.js
+++ homework-classes/App.js
@@ -50,55 +50,83 @@ class App {
       const repos = await Util.fetchJSON(url);
       this.repos = repos.map(repo => new Repository(repo));
 
-      // TODO: add your own code here
-    } catch (error) {
-      this.renderError(error);
+      // TODO: add your own code here 
+      //SORT repo names in alphabetical order (case-insensitive) & generate obj with REDUCE
+
+      let sorted = Object.keys(this.repos)
+        .sort((a, b) => {
+          a = this.repos[a].repository.name.toLowerCase();
+          b = this.repos[b].repository.name.toLowerCase();
+          if (a === b) return 0;
+          return a < b ? -1 : 1;
+        })
+        .reduce((acc, curr, i) => {
+          acc[i] = this.repos[curr].repository.name;
+          return acc;
+        }, {});
+
+      // generate ARRAY of repo names
+      let repoNamesArr = [];
+      for (let i in sorted) {
+        repoNamesArr.push(sorted[i]);
+
+        // create list of repo names from Arr to select from
+        repoNamesArr.forEach(repoName => Util.createAndAppend('option', select, {
+          id: 'repoList',
+          text: repoName,
+          value: repoNamesArr.indexOf(repoName)
+        }));
+
+
+
+      } catch (error) {
+        this.renderError(error);
+      }
+    }
+
+    /**
+     * Removes all child elements from a container element
+     * @param {*} container Container element to clear
+     */
+    clearContainer() {
+      while (this.mainContainer.firstChild) {
+        this.mainContainer.removeChild(this.mainContainer.firstChild);
+      }
+    }
+
+    /**
+     * Fetch contributor information for the selected repository and render the
+     * repo and its contributors as HTML elements in the DOM.
+     * @param {object} repo The selected repository object
+     */
+    async selectRepository(repo) {
+      try {
+        this.clearContainer();
+        const contributors = await repo.fetchContributors();
+
+        const repoContainer = Util.createAndAppend('div', this.mainContainer);
+        const contributorContainer = Util.createAndAppend('div', this.mainContainer);
+
+        const contributorList = Util.createAndAppend('ul', contributorContainer);
+
+        repo.render(repoContainer);
+
+        contributors
+          .map(contributor => new Contributor(contributor))
+          .forEach(contributor => contributor.render(contributorList));
+      } catch (error) {
+        this.renderError(error);
+      }
+    }
+
+    /**
+     * Render an error to the page.
+     * @param {Error} error An Error object describing the error.
+     */
+    renderError(error) {
+      console.error(error); // TODO: replace with your own code
     }
   }
 
-  /**
-   * Removes all child elements from a container element
-   * @param {*} container Container element to clear
-   */
-  clearContainer() {
-    while (this.mainContainer.firstChild) {
-      this.mainContainer.removeChild(this.mainContainer.firstChild);
-    }
-  }
-
-  /**
-   * Fetch contributor information for the selected repository and render the
-   * repo and its contributors as HTML elements in the DOM.
-   * @param {object} repo The selected repository object
-   */
-  async selectRepository(repo) {
-    try {
-      this.clearContainer();
-      const contributors = await repo.fetchContributors();
-
-      const repoContainer = Util.createAndAppend('div', this.mainContainer);
-      const contributorContainer = Util.createAndAppend('div', this.mainContainer);
-
-      const contributorList = Util.createAndAppend('ul', contributorContainer);
-
-      repo.render(repoContainer);
-
-      contributors
-        .map(contributor => new Contributor(contributor))
-        .forEach(contributor => contributor.render(contributorList));
-    } catch (error) {
-      this.renderError(error);
-    }
-  }
-
-  /**
-   * Render an error to the page.
-   * @param {Error} error An Error object describing the error.
-   */
-  renderError(error) {
-    console.error(error); // TODO: replace with your own code
-  }
-}
-
-const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
-window.onload = () => new App(HYF_REPOS_URL);
+  const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+  window.onload = () => new App(HYF_REPOS_URL);
